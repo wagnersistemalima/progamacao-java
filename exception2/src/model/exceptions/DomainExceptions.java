c2ab51bc4f0c28597f7fa5759e739de9341cc176
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-08-29 20:59:03 -0300

    Good solution

---
+++ exception2/src/model/exceptions/DomainExceptions.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class DomainExceptions extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public DomainExceptions(String msg) {
+		super(msg);
+	}
+
+}
