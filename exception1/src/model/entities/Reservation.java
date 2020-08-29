b10ce19d98b171418d2bd133466aa394bea82015
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-08-29 14:06:37 -0300

    versão ruim

--- exception1/src/model/entities/Reservation.java
+++ exception1/src/model/entities/Reservation.java
@@ -43,9 +43,18 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS);
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Error in reservation: Reservation dates for update must be future dates";
+		}
+		if(! checkOut.after(checkIn)) {
+			return "Error in reservation: Check-out date must be after check-in date";
+		}
+		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;                                                     // se não deu erro retorna nullo
 	}
 	
 	@Override
